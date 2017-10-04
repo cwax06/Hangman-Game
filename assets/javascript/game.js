eeac6b0825c06110751db09583d6589a12495d67
Andrew Jones <Andrew@AHJones-iMac.local>
2017-10-03 21:48:51 -0600

    Activities Code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,30 @@
 
-// Repo of possible answers
+// Array of possible answers
 
+var characters = ["Bob Sacamano", "Cousin Jeffrey", "Jack Klompus", "Jerry Seinfeld",
+    "Elaine Benes", "Cosmo Kramer", "George Costanza",];
 
-// Randomly select a possible answer from repo
+// Computer randomly select a possible answer from array
 
+for (var i = 0; i < characters.length; i++) {
+    console.log("Do you know " + characters[i]);
+}
 
 // Listen for user input
 
 
+
+document.onkeyup = function (event) {
+    userText.textContent = event.key;
+};
+
+var userText = document.getElementById("user-text");
+
 // Compare user input with randomly selected answer
 
+if (characters[j].charAt(0) === "c" || characters[j].charAt(0) === "o") {
+    alert("Starts with a c or an o!");
+}
 
 // Display correct letters when selected
 
@@ -17,7 +32,7 @@
 // Display all guesses below
 
 
-// All letters guessed that are NOT a part of answer is a strike (up to 5)
+// All letters guessed that are NOT a part of answer is a strike (up to 10)
 
 
 // When all correct letters have been guessed, play celebration
