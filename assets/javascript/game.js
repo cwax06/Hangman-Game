5b8a07e99f004b911b26f484a82f900e63d55261
Andrew Jones <andrew@Andrews-MacBook-Pro.local>
2017-10-03 19:41:14 -0600

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+// Repo of possible answers
+
+
+// Randomly select a possible answer from repo
+
+
+// Listen for user input
+
+
+// Compare user input with randomly selected answer
+
+
+// Display correct letters when selected
+
+
+// Display all guesses below
+
+
+// All letters guessed that are NOT a part of answer is a strike (up to 5)
+
+
+// When all correct letters have been guessed, play celebration
+
+
+// Restart the game
